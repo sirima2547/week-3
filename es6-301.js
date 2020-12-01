9b6a648bfc9427fb480eb469ea5e5740d9408aad
sirima2547 <69945455+sirima2547@users.noreply.github.com>
2020-12-01 11:22:12 +0700

    Add files via upload

---
+++ es6-301.js
@@ -0,0 +1,17 @@
+//The Strihg
+//1.The basic Synyax of  String
+//let message ="Hello World!";
+//console.log("message");
+//2.The basic Synyax of  String
+let fname ="Mark";
+let lname ="Sakaberg";
+let fullName =fname+" "+lname ;
+//console.log(fname+ " " +lname);
+console.log(fullName);
+//3.The Strihg with number
+console.log("HEllo" + 12) ;//Hello12
+console.log("10" + 2 );//12
+
+console.log("10" == 10);//true
+console.log("10" === 10);//false
+console.log("10"!==10);//
